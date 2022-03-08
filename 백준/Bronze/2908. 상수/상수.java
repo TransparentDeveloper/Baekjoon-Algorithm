fc30aa851069293a776a782f034e2f2723170f13
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2022-03-08 17:26:55 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -2,35 +2,37 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
- 
 	public static void main(String[] args) throws IOException{
-		Scanner scanner = new Scanner(System.in);
-		
-		int num1 = scanner.nextInt();
-		int num2 = scanner.nextInt();
-		
-		num1 =reverse(num1);
-		num2 = reverse(num2);
-		
-		System.out.println(bigger(num1,num2));
-		
-	}
-	
-	static int bigger(int su1, int su2) {
-		int res= su1;
-		if(res<su2)
-			res=su2;
-		return res;
-	}
-	
-	static int reverse(int su) {
-		int res=0;
-		while(su != 0) {
-			res = res*10 + su%10;
-			su = su/10;
+		int num1 = 0;
+		int num2 = 0;
+		int input;
+		int za_li = 1;
+		while(true) {
+			input = System.in.read();
+			
+			if(input == 10 || input == 32)
+				break;
+			if(47<input&&input<58) {
+				input = input-48;
+				num1 = za_li*input+num1;
+				za_li *= 10;
+			}
+		}
+		za_li = 1;
+		while(true) {
+			input = System.in.read();
+			
+			if(input == 10 || input == 32)
+				break;
+			if(47<input&&input<58) {
+				input = input-48;
+				num2 = za_li*input+num2;
+				za_li *= 10;
+			}
 		}
 		
-		return res;
+		int res = (num1>num2) ? num1: num2;
+		System.out.println(res);
 	}
 	
 }
