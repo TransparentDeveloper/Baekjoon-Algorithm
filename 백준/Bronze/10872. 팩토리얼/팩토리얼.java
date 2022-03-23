da5dd952cf881711bdc7b92e5a41bb4697a1bbe2
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2022-03-23 19:27:07 +0900

    [Bronze III] Title: 팩토리얼, Time: 204 ms, Memory: 17568 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,20 +1,18 @@
 import java.io.IOException;
-import java.util.Scanner;
-
-public class Main {
-
-	static boolean[] not_prime = new boolean[10001];
-
-	public static void main(String[] args) throws IOException {
-		Scanner s = new Scanner(System.in);
-		int input = s.nextInt();
-		s.close();
-		System.out.println(factorial(input));
-	}
+	import java.util.Scanner;
 	
-	static int factorial(int n) {
-		if(n==1 || n==0)
-			return 1;
-		return n*factorial(n-1);
+	public class Main {
+	
+		public static void main(String[] args) throws IOException {
+			Scanner s = new Scanner(System.in);
+			int input = s.nextInt();
+			s.close();
+			System.out.println(factorial(input));
+		}
+		
+		static int factorial(int n) {
+			if(n==1 || n==0)
+				return 1;
+			return n*factorial(n-1);
+		}
 	}
-}
