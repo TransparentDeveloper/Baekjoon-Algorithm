0c26b13d71a64ba0303843468ea146e3a84851bd
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-01-23 17:46:18 +0900

    [Silver V] Title: 단어 정렬, Time: 348 ms, Memory: 26728 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -8,34 +8,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        // key 는 문자열 길이, value 는 key 만큼의 길이를 가지는 문자열 list
-        Map<Integer,List<String>> hashMap = new HashMap<>();
-
-        String str;
-        for(int i=0; i< N; i++){
-            str = br.readLine();
-            // 같은 길이의 문자열이 이미 저장되어 있다면
-            if(hashMap.containsKey(str.length())){
-                List<String> stringList = hashMap.get(str.length());
-                stringList.add(str);
-                Collections.sort(stringList);
-                hashMap.replace(str.length(),stringList);
-            }
-            // 특정 길이의 문자열이 map 안에 없다면
-            else{
-                List<String> stringList = new ArrayList<>();
-                stringList.add(str);
-                hashMap.put( str.length(),stringList);
-            }
+        String[] strings = new String[N];
+        for(int i=0; i<N; i++){
+            strings[i] = br.readLine();
         }
         br.close();
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<51;i++){
-            if(hashMap.containsKey(i)){
-                List<String> stringList = hashMap.get(i);
-                stringList.stream().distinct().forEach(s->sb.append(s).append('\n'));
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length())
+                    return o1.compareTo(o2);
+                return o1.length()-o2.length();
             }
+        });
+        StringBuilder sb = new StringBuilder();
+        sb.append(strings[0]).append('\n');
+        for(int i=1;i<N;i++){
+            if(strings[i].equals(strings[i-1]))
+                continue;
+            sb.append(strings[i]).append('\n');
         }
+
         System.out.println(sb);
     }
 }
