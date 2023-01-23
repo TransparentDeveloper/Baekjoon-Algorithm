6768ac042d1d8df28ae8439e10a1eac4bb7bc1d2
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-01-23 15:27:51 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1640 ms, Memory: 336908 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] cnt = new int[10000001];
+        int[] cnt = new int[10001];
 
         int max_num=0;
         int temp;
