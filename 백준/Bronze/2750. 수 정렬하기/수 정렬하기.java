e2ebae3716b898ea6f0730c63e9a0570fee2d013
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-01-23 16:07:30 +0900

    [Bronze II] Title: 수 정렬하기, Time: 136 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,23 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.io.InputStreamReader;
-	
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		Integer[] arr = new Integer[N];
+import java.util.*;
 
-		while(N-->0) 
-			arr[N] = Integer.parseInt(br.readLine());
-		
-		Arrays.sort(arr);
-		
-		for(int i=0; i<arr.length; i++) 
-			System.out.println(arr[i]);
-		
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] cnt = new int[2001];
+
+        int temp;
+        for(int i=0; i< N;i++){
+            temp = Integer.parseInt(br.readLine());
+            // [-1000 , -1] -> [0 , 999]
+            // [0, 1000] -> [1000, 2000]
+            temp += 1000;
+            cnt[temp]++;
+        }
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<2001;i++){
+            if(cnt[i]==0)
+                continue;
+            sb.append(i-1000).append('\n');
+        }
+        System.out.println(sb);
+    }
 }
