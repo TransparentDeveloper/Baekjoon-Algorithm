cb1b5d6c27d04cf7aac823b7107435f77d46f741
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-07-03 15:10:26 +0900

    [level 3] Title: 가장 먼 노드, Time: 113.63 ms, Memory: 28.5 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,34 +1,33 @@
 from collections import defaultdict
-import heapq
-
-# {
-#     3: [(6, 1), (4, 1), (2, 1), (1, 1)], 
-#     6: [(3, 1)], 
-#     4: [(3, 1), (2, 1)], 
-#     2: [(3, 1), (1, 1), (4, 1), (5, 1)], 
-#     1: [(3, 1), (2, 1)], 
-#     5: [(2, 1)]
-# }
-
+import heapq as hq
 
 def solution(n, edge):
+    
     graph = defaultdict(list)
     for n1,n2 in edge:
-        graph[n1].append( (n2,1) )
-        graph[n2].append( (n1,1) )
+        graph[n1].append((1,n2))
+        graph[n2].append((1,n1))
     
-    distances = {node: float('inf') for node in graph} 
+    distances = {node: float('inf') for node in graph.keys()}
     distances[1] = 0
-    queue = []
-    heapq.heappush(queue, [distances[1],1] )
-    
-    while queue:
-        current_distance, current_node = heapq.heappop(queue);      
-        for next_node,next_distance in graph[current_node]:
-            if (next_distance+distances[current_node] < distances[next_node]):
-                distances[next_node] = next_distance+distances[current_node]
-                heapq.heappush(queue,[distances[next_node],next_node])
+    q = []
+    hq.heappush(q,(0,1))
+    while q:
+        curWei,curNod = hq.heappop(q)
+        for nxtWei,nxtNod in graph[curNod]:
+            if curWei+nxtWei < distances[nxtNod]:
+                distances[nxtNod] = curWei+nxtWei
+                hq.heappush(q, (distances[nxtNod], nxtNod))
     
     max_distance = max(distances.values())
-    answer = list(filter(lambda x:max_distance==x[1],distances.items()))
-    return len(answer)
+    return len(list(filter(lambda x: max_distance == x, distances.values())))
+
+
+# {
+#     3: [(1, 6), (1, 4), (1, 2), (1, 1)], 
+#     6: [(1, 3)], 
+#     4: [(1, 3), (1, 2)], 
+#     2: [(1, 3), (1, 1), (1, 4), (1, 5)], 
+#     1: [(1, 3), (1, 2)], 
+#     5: [(1, 2)]
+# }
