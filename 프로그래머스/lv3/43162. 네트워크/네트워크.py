957bf83545a0037ac212de46673efcbe1e020161
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-07-05 15:29:22 +0900

    [level 3] Title: 네트워크, Time: 0.42 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,28 +1,27 @@
-from collections import defaultdict
-
-def findParent(x):
-    if parent[x] != x:
-        parent[x] = findParent(parent[x])
-    return parent[x]
-
-def union(x,y):
-    x = findParent(x)
-    y = findParent(y)
-    if x < y :
-        parent[y] = x
-    else:
-        parent[x] = y
-        
-
 def solution(n, computers):
-    answer = n
     global parent
-    parent = [ i for i in range(n+1)]
-    
+    parent = [ i for i in range(n)] 
+    answer = n
     for i in range(len(computers)):
         for j in range(len(computers[i])):
+            if i == j: continue
             if computers[i][j] == 1:
                 if findParent(i) != findParent(j):
                     union(i,j)
-                    answer-=1
+                    answer -= 1
     return answer
+
+def findParent(x):
+    if x != parent[x]:
+        parent[x] = findParent(parent[x])
+    return parent[x]
+
+def union(a,b):
+    a = findParent(a)
+    b = findParent(b)
+    
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
+    return
