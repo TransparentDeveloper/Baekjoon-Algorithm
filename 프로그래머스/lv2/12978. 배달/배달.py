b97cfb362022198626000bf1dc04248bc905c294
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-07-05 14:47:00 +0900

    [level 2] Title: 배달, Time: 0.16 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.py
+++ 프로그래머스/lv2/12978. 배달/배달.py
@@ -2,19 +2,22 @@ from collections import defaultdict
 import heapq as hq
 
 def solution(N, road, K):
-    dic = defaultdict(list)
+    dict = defaultdict(list)
     for n1,n2,w in road:
-        dic[n1].append((w,n2))
-        dic[n2].append((w,n1))
-    distances = {node: float('inf') for node in range(1,N+1)}
+        dict[n1].append((w,n2))
+        dict[n2].append((w,n1))
+        
+    distances = {node: float('inf') for node in dict }
     distances[1] = 0
-    q = []
-    hq.heappush(q,(0,1))
-    while q:
-        curWei,curNod = hq.heappop(q)
-        for nexWei,nexNod in dic[curNod]:
-            if nexWei+curWei < distances[nexNod]:
-                distances[nexNod] = nexWei+curWei
-                hq.heappush(q,(nexWei+curWei,nexNod))
-    print(distances)
+    heap = []
+    hq.heappush(heap,(0,1))
+    
+    while heap:
+        curWei,curNod = hq.heappop(heap)
+        for nexWei,nexNod in dict[curNod]:
+            if nexWei + curWei < distances[nexNod]:
+                distances[nexNod] = nexWei + curWei
+                hq.heappush(heap,(nexWei + curWei, nexNod))
+    
     return len(list(filter(lambda x:x[1]<=K,distances.items())))
+    
