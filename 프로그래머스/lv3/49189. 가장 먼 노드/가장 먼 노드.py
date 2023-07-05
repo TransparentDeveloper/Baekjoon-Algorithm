ad489d3608f04ba8242066687f6ac4a1af4e140f
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-07-05 15:20:16 +0900

    [level 3] Title: 가장 먼 노드, Time: 84.37 ms, Memory: 23.2 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -2,32 +2,22 @@ from collections import defaultdict
 import heapq as hq
 
 def solution(n, edge):
-    
-    graph = defaultdict(list)
+    dic = defaultdict(list)
     for n1,n2 in edge:
-        graph[n1].append((1,n2))
-        graph[n2].append((1,n1))
+        dic[n1].append(n2)
+        dic[n2].append(n1)
     
-    distances = {node: float('inf') for node in graph.keys()}
+    distances = {node: float('inf') for node in range(n+1)}
     distances[1] = 0
-    q = []
-    hq.heappush(q,(0,1))
-    while q:
-        curWei,curNod = hq.heappop(q)
-        for nxtWei,nxtNod in graph[curNod]:
-            if curWei+nxtWei < distances[nxtNod]:
-                distances[nxtNod] = curWei+nxtWei
-                hq.heappush(q, (distances[nxtNod], nxtNod))
+    heap = []
+    hq.heappush(heap,(0,1))
+    while heap:
+        curWei,curNod = hq.heappop(heap)
+        for nxtNod in dic[curNod]:
+            if curWei+1 < distances[nxtNod]:
+                distances[nxtNod] = curWei+1
+                hq.heappush(heap,(curWei+1,nxtNod))
+    del distances[0]
+    maxDistance = max(distances.values())
+    return len(list(filter(lambda x : x == maxDistance,distances.values())))
     
-    max_distance = max(distances.values())
-    return len(list(filter(lambda x: max_distance == x, distances.values())))
-
-
-# {
-#     3: [(1, 6), (1, 4), (1, 2), (1, 1)], 
-#     6: [(1, 3)], 
-#     4: [(1, 3), (1, 2)], 
-#     2: [(1, 3), (1, 1), (1, 4), (1, 5)], 
-#     1: [(1, 3), (1, 2)], 
-#     5: [(1, 2)]
-# }
