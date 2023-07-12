f4812d31aa5a69ce26acd9b080040462efa4601f
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-07-12 11:21:27 +0900

    [level 3] Title: 네트워크, Time: 0.61 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,27 +1,26 @@
 def solution(n, computers):
     global parent
-    parent = [ i for i in range(n)] 
-    answer = n
+    parent = [i for i in range(len(computers))]
+    answer = len(computers)
     for i in range(len(computers)):
-        for j in range(len(computers[i])):
+        for j in range(len(computers[0])):
             if i == j: continue
-            if computers[i][j] == 1:
-                if findParent(i) != findParent(j):
-                    union(i,j)
-                    answer -= 1
+            elif computers[i][j]:
+                if findParent(i)!=findParent(j):
+                    union(i,j);
+                    answer-=1
     return answer
+
 
 def findParent(x):
     if x != parent[x]:
         parent[x] = findParent(parent[x])
     return parent[x]
 
-def union(a,b):
-    a = findParent(a)
-    b = findParent(b)
-    
-    if a<b:
-        parent[b] = a
-    else:
-        parent[a] = b
-    return
+def union(x,y):
+    x = findParent(x)
+    y = findParent(y)
+    if x<y:
+        parent[y] = x
+    elif y<x:
+        parent[x] = y
