43bc8c11b16886c0874f60b7b307af18f0afba12
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-08-03 10:37:01 +0900

    [level 3] Title: 입국심사, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -1,28 +1,20 @@
 def solution(n, times):
-    global N,Times
-    N = n
-    Times = times
     left = 1
     right = max(times)*n
     answer = 0
     while left <= right:
-        # print(answer)
-        mid = (left+right)//2
+        mid = (left+ right)//2
         
-        if isPossible(mid): 
-            # n명을 mid 시간동안 해결 가능하다면 그보다 더 적은 시간 안에서도 해결 가능하지 않을까?
-            answer = mid
-            right = mid-1
+        cnt = 0
+        # mid 시간 동안 각 심사관이 심사할 수 있는 사람의 수
+        for time in times:
+            cnt += mid//time
+        # 최소시간을 구해라
+        if cnt >= n:
+            answer = mid 
+            right = mid - 1
         else:
             left = mid + 1
+        
+    
     return answer
-
-
-
-
-# n명의 사람을 mid 시간 안에서 해결 가능한지 판단 (해결 가능하면 True)
-def isPossible(mid):
-    passed = 0 # mid시간 동안 심사를 통과한 모든 사람
-    for time in Times:
-        passed += mid // time
-    return passed>=N
