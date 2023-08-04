a2e95f0d75be4a6b4f86e3cfe46262302d35d277
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-08-04 11:20:50 +0900

    [level 3] Title: 가장 먼 노드, Time: 136.48 ms, Memory: 22.5 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -2,20 +2,20 @@ from collections import defaultdict
 import heapq as hq
 
 def solution(n, edge):
-    di = defaultdict(list);
-    for n1,n2 in edge:
-        di[n1].append(n2);
-        di[n2].append(n1);
-    distances = {i: float('inf') for i in range(1,n+1)}
-    distances[1] = 0
-    heap = list()
+    di = defaultdict(list)
+    for s,e in edge:
+        di[s].append(e)
+        di[e].append(s)
+    distances = {i: float('inf') for i in di}
+    distances[1] = 0 
+    heap = []
     hq.heappush(heap,(0,1))
     while heap:
-        curWei,curNod = hq.heappop(heap)
-        for nxtNod in di[curNod]:
-            if curWei + 1 < distances[nxtNod] :
-                distances[nxtNod] = curWei + 1
-                hq.heappush(heap,(curWei + 1,nxtNod))
-    maxWei = max(distances.values())
-    return len(list(filter(lambda x: x==maxWei,distances.values())))
+        curWei, curPos = hq.heappop(heap)
+        for nxtPos in di[curPos]:
+            if curWei + 1 < distances[nxtPos]:
+                distances[nxtPos] = curWei+1
+                hq.heappush(heap ,(distances[nxtPos], nxtPos))
+    maxVal = max(distances.values())
+    return len(list(filter(lambda x : x == maxVal,distances.values())))
         
