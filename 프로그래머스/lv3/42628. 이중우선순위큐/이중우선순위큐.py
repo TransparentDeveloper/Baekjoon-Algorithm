aa2277d8ebe5f521a2e4e7fb2826a6c6da13f1fb
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-08-12 15:40:41 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,30 +1,20 @@
 import heapq as hq
-
 def solution(operations):
-    maxli = []
-    minli = []
-    elCnt = 0
-
-    for oper in operations:
-        if elCnt == 0 and (oper == "D -1" or oper == "D 1"):
-            continue
-        if oper == "D -1":
-            elCnt-=1
-            hq.heappop(minli)
-        elif oper == "D 1":
-            elCnt-=1
-            hq.heappop(maxli)
-        else:
-            elCnt+=1
-            num = int(oper.split(" ")[1])
-            hq.heappush(maxli,num*(-1))
-            hq.heappush(minli,num)
-            
-    newli = [el*(-1) for el in maxli if el*(-1) in minli]
-
-    if not newli or len(newli)==1:
-        return [0,0]
     
-    newli.sort(key=lambda x: x)
-    return [newli[-1],newli[0]]
+    heap = []
+    for oper in operations:
+        op,da = oper.split()
+        da = int(da)
         
+        if op=='I':
+            hq.heappush(heap,da)
+        else:
+            if da==1 and heap:
+                maxNum = max(heap)
+                heap.remove(maxNum)
+                hq.heapify(heap)
+            elif da == -1 and heap:
+                hq.heappop(heap)
+    if not heap:
+        return [0,0]
+    return [max(heap),min(heap)]
