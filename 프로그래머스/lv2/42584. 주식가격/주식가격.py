3b7646ed9f27e9561b4aa2239b22d9e5b8d6f74a
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-08-24 14:15:29 +0900

    [level 2] Title: 주식가격, Time: 18.99 ms, Memory: 17.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -0,0 +1,12 @@
+def solution(prices):
+    answer = [i for i in range(len(prices)-1,-1,-1)] # 주식가격이 안떨어진다고 가정하고 초기화
+    stack = []
+    for idx,val in enumerate(prices):
+        if not stack or stack[-1][0] <= val:
+            stack.append((val,idx))
+        else:
+            while stack and stack[-1][0] > val:
+                preVal,preIdx = stack.pop()
+                answer[preIdx] = idx-preIdx
+            stack.append((val,idx))
+    return answer
