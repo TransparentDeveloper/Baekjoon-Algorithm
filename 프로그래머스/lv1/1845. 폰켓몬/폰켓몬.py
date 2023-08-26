b144a8752e97e7a9f2e781031e4b071fe67a462d
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-08-26 18:09:33 +0900

    [level 1] Title: 폰켓몬, Time: 0.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,12 +1,3 @@
-
 def solution(nums):
-    dic = dict()
-    for nu in nums:
-        dic[nu] = dic.get(nu,0) + 1
-        
-    select_num = len(nums)//2
-    kind_num = len(dic.keys())
-    
-    if select_num <= kind_num:
-        return select_num
-    return kind_num
+    answer = len(set(nums)) 
+    return answer if answer<=len(nums)//2 else len(nums)//2
