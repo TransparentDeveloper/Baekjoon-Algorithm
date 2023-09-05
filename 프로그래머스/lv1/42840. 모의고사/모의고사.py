282a9872a5c4a0482be0799bf1f5673d02f91a51
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-09-05 10:55:19 +0900

    [level 1] Title: 모의고사, Time: 3.37 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,16 +1,21 @@
 def solution(answers):
-    one = [1,2,3,4,5] * (10000//5)
-    two = [2, 1, 2, 3, 2, 4, 2, 5] * (10000//8)
-    three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * (10000//10)
-    di = dict()
-    for idx,correct in enumerate(answers):
-        if one[idx] == correct:
-       		di[1] = di.get(1,0) + 1
-        if two[idx] == correct:
-            di[2] = di.get(2,0) + 1
-        if three[idx] == correct:
-        	di[3] = di.get(3,0) + 1
-    maxScore = max(di.values())
-    answer = sorted(di.items(),key = lambda x: (x[1],x[0]))
-    answer = [i[0] for i in answer if maxScore == i[1]]
-    return answer
+    answer = []
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
+
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx%len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[idx%len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[idx%len(pattern3)]:
+            score[2] += 1
+    
+    result = []
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
+
+    return result
