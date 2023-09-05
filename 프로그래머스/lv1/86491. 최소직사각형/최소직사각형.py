14e873e16a2fae7b92afc760c5254bc924d17162
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-09-05 10:03:12 +0900

    [level 1] Title: 최소직사각형, Time: 5.38 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,16 +1,14 @@
 def solution(sizes):
-    newList = []
-    for s in sizes:
-        if s[0] < s[1]:
-            newList.append([s[0],s[1]])
-        else:
-            newList.append([s[1],s[0]])
-    maxWidth = 0
-    maxHeight = 0
-    for w,h in newList:
-        if w > maxWidth:
-            maxWidth=w
-        if h > maxHeight:
-            maxHeight=h
+    # 큰 값을 가로로, 작은 값을 세로로
     
-    return maxWidth * maxHeight
+    maxWidth = -1
+    maxHeight = -1
+    for width,height in sizes:
+        if width >= height:
+            maxWidth = max(maxWidth,width)
+            maxHeight = max(maxHeight,height)
+        else:
+            maxWidth = max(maxWidth,height)
+            maxHeight = max(maxHeight,width)
+            
+    return (maxHeight*maxWidth)
