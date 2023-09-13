c8d282a53a9c7c8db182db0fab87a5c5c7f7d0fd
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-09-13 15:53:00 +0900

    [level 3] Title: 네트워크, Time: 2.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -0,0 +1,22 @@
+function solution(n, computers) {
+    const visited = new Array(n).fill(false)
+    
+    function DFS(curIdx){
+        visited[curIdx] = true
+        for(let nxtIdx =0 ; nxtIdx<n;nxtIdx++){
+            if(!visited[nxtIdx] && computers[curIdx][nxtIdx] == 1){
+                DFS(nxtIdx)
+            }
+        }
+    }
+    let answer = 0
+    for(let i =0; i<n;i++){
+        if (!visited[i]){
+            console.log()
+            DFS(i)
+            answer+=1
+        }
+    }
+    
+    return answer;
+}
