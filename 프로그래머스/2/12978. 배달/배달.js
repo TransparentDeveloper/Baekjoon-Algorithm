1011ee2556484ccdf85f5e976bd2e5215e773004
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2024-10-16 12:47:57 +0900

    [level 2] Title: 배달, Time: 11.04 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,30 +1,35 @@
-const INF = 1000000
-function solution(N, road, K) {
-    
-    const obj = {}
+const UPPER_LIMIT = 500001
 
+function solution(N, road, K) {
+    const obj = {}
     road.forEach(([s,e,c])=>{
-        if(s in obj) obj[s].push([e,c])
-        else obj[s] = [[e,c]]
+        if(s in obj) 
+            obj[s].push([c,e])
+        else 
+            obj[s] = [[c,e]]
+        if(e in obj) 
+            obj[e].push([c,s])
+        else 
+            obj[e] = [[c,s]]
         
-        if(e in obj) obj[e].push([s,c])
-        else obj[e] = [[s,c]]
     })
     
-    const distance = new Array(N+1).fill(INF)
-    distance[1] = 0
-    const que = [[1,0]]
+    const distances = new Array(N+1).fill(UPPER_LIMIT)
+    const que = []
+    
+    distances[1] = 0
+    que.push([0,1])
     
     while(que.length){
-        const [curNode, curCost] = que.pop()    
-
-        obj[curNode].forEach(([nxtNode, nxtCost])=>{
-            if(curCost + nxtCost < distance[nxtNode]){
-                distance[nxtNode] = curCost + nxtCost
-                que.push([nxtNode, curCost + nxtCost])
+        que.sort((e1,e2)=> e2[0] - e1[0])
+        const [curCost, curNode] = que.pop()
+        obj[curNode].forEach(([nxtCost, nxtNode])=>{
+            if(curCost + nxtCost < distances[nxtNode]){
+                distances[nxtNode] = curCost + nxtCost
+                que.push([curCost+nxtCost,nxtNode])
             }
         })
     }
-
-    return distance.filter((dist) => dist<=K).length
+    
+    return distances.filter((distance)=> distance <= K).length
 }
