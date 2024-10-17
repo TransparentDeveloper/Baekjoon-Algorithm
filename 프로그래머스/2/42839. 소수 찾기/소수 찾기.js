54576372eace2d97a4bee01e3c343d5c2dd885af
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2024-10-17 14:10:40 +0900

    [level 2] Title: 소수 찾기, Time: 1550.79 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,58 +1,50 @@
 const UPPER_LIMIT = 10000000
 
-function storeIsPrime(){
-    const isPrime = new Array(UPPER_LIMIT+1).fill(true)
-    
+const isPrime = new Array(UPPER_LIMIT+1).fill(true)
+
+function initIsPrime(){
+    const sqrt = Math.floor(Math.sqrt(UPPER_LIMIT))
+
     isPrime[0] = false
     isPrime[1] = false
-    
-    const sqrt = Math.floor(Math.sqrt(UPPER_LIMIT))
-    for(let i = 2; i<=sqrt; i++){
+
+    for(let i = 2; i<=sqrt; i++) {
         if(!isPrime[i]) continue
         for(let j = i*i; j<=UPPER_LIMIT; j+=i)
             isPrime[j] = false
-    }
-    
-    return () => isPrime
+    }   
 }
+
 
 function solution(numbers) {
-    const getIsPrime = storeIsPrime()
-    const isPrime = getIsPrime()
-    
-    const subNumbers = getAllSubNumbers(numbers)
-    const primes = subNumbers.filter((subNumber)=>isPrime[subNumber])
-    return primes.length;
+    initIsPrime()
+    const nums = getAllNums(numbers)
+    return nums.filter((num)=>isPrime[num]).length
 }
 
-function getAllSubNumbers(numbers){
-    const size = numbers.length
-    const splited = numbers.split("")
-    const isVisited = new Array(size).fill(false)
+function getAllNums(numbers){
+    const split = numbers.split("")
+    const splitSize = split.length
     
-    const answerSet = new Set()
-    
-    function dfs(depth, subNumbers){
-        
-        if(depth === size){
-            if(subNumbers !== '') 
-                answerSet.add(parseInt(subNumbers))
+    const set = new Set()
+    const isVisited = new Array(splitSize).fill(false)
+    function DFS(depth, nums){
+        if(splitSize == depth){
+            if(nums!=='') set.add(parseInt(nums))
             return
         }
         
-        for(let i = 0; i < size; i++){
-            if(isVisited[i])continue
+        for(let i = 0; i<splitSize; i++){
+            if(isVisited[i]) continue
             
-            // 1. 지금 걸 가져간다.
             isVisited[i] = true
-            dfs(depth+1, subNumbers+splited[i])
+            DFS(depth+1, nums+split[i])
             isVisited[i] = false
             
-            // 2. 놔두고 간다.
-            dfs(depth+1, subNumbers)
+            DFS(depth+1, nums)
         }
     }
     
-    dfs(0,'')
-    return Array.from(answerSet)
+    DFS(0,'')
+    return Array.from(set)
 }
