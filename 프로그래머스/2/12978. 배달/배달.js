48f16e6ad680c72302ac99ce62cbc25a9e1a0341
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2024-10-17 13:46:51 +0900

    [level 2] Title: 배달, Time: 13.33 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,35 +1,83 @@
-const UPPER_LIMIT = 500001
+class MinHeap{
+    constructor(){
+        this.heap = []
+    }
+    bubbleUp(idx){
+        const parentIdx = Math.floor((idx-1) / 2)
+        
+        if(idx<=0) return
+        if(this.heap[idx][0] < this.heap[parentIdx][0]){
+            let tmp = this.heap[parentIdx]
+            this.heap[parentIdx] = this.heap[idx]
+            this.heap[idx] = tmp
+            this.bubbleUp(parentIdx)
+        }
+    }
+    bubbleDown(idx){
+        const leftIdx = idx*2 + 1
+        const rightIdx = idx*2 + 2
+        
+        let childIdx = idx
+        if(leftIdx < this.heap.length && this.heap[leftIdx][0]<this.heap[childIdx][0])
+            childIdx = leftIdx
+        if(rightIdx < this.heap.length && this.heap[rightIdx][0]<this.heap[childIdx][0])
+            childIdx = rightIdx
+        
+        if(childIdx == idx) return
+        
+        const tmp = this.heap[idx]
+        this.heap[idx] = this.heap[childIdx]
+        this.heap[childIdx] = tmp
+        
+        this.bubbleDown(childIdx)
+        
+        
+    }
+    heappush(pair){
+        this.heap.push(pair)
+        this.bubbleUp(this.heap.length-1)
+    }
+    heappop(){
+        if(this.heap.length==0) return null
+        if(this.heap.length==1) return this.heap.pop()
+        
+        const answer = this.heap[0]
+        this.heap[0] = this.heap.pop()
+        this.bubbleDown(0)
+        return answer
+    }
+    getSize(){
+        return this.heap.length
+    }
+}
 
 function solution(N, road, K) {
     const obj = {}
-    road.forEach(([s,e,c])=>{
-        if(s in obj) 
-            obj[s].push([c,e])
-        else 
-            obj[s] = [[c,e]]
-        if(e in obj) 
-            obj[e].push([c,s])
-        else 
-            obj[e] = [[c,s]]
+    
+    road.forEach(([a,b,c])=>{
+        if(!obj[a]) obj[a] = []
+        if(!obj[b]) obj[b] = []
         
+        obj[a].push([c,b])
+        obj[b].push([c,a])
     })
     
-    const distances = new Array(N+1).fill(UPPER_LIMIT)
-    const que = []
-    
+    const heap = new MinHeap()
+    const distances = new Array(N+1).fill(10001*N)
     distances[1] = 0
-    que.push([0,1])
+    heap.heappush([0,1])
     
-    while(que.length){
-        que.sort((e1,e2)=> e2[0] - e1[0])
-        const [curCost, curNode] = que.pop()
+    while(heap.getSize()){
+        const [curCost, curNode] = heap.heappop()
+        
         obj[curNode].forEach(([nxtCost, nxtNode])=>{
-            if(curCost + nxtCost < distances[nxtNode]){
-                distances[nxtNode] = curCost + nxtCost
-                que.push([curCost+nxtCost,nxtNode])
+            if(nxtCost+curCost < distances[nxtNode]){
+                distances[nxtNode] = nxtCost+curCost
+                heap.heappush([nxtCost+curCost,nxtNode])
             }
         })
     }
     
-    return distances.filter((distance)=> distance <= K).length
+    const answer = distances.filter((distance)=>distance<=K).length
+    return answer
 }
