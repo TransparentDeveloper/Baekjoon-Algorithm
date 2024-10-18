cc6fec904a0c308b4ff4c1a6bab10f46360360a5
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2024-10-18 16:03:06 +0900

    [level 2] Title: 배달, Time: 8.40 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,83 +1,76 @@
-class MinHeap{
+const COST_UPPER_LIMIT=100000000000
+
+class Heap{
     constructor(){
-        this.heap = []
+        this.heap=[]
     }
     bubbleUp(idx){
-        const parentIdx = Math.floor((idx-1) / 2)
-        
-        if(idx<=0) return
-        if(this.heap[idx][0] < this.heap[parentIdx][0]){
-            let tmp = this.heap[parentIdx]
-            this.heap[parentIdx] = this.heap[idx]
-            this.heap[idx] = tmp
+        const parentIdx=Math.floor((idx-1)/2)
+        if(0<parentIdx&&this.heap[idx][0]<this.heap[parentIdx][0]){
+            [this.heap[idx],this.heap[parentIdx]]=[this.heap[parentIdx],this.heap[idx]]
             this.bubbleUp(parentIdx)
         }
     }
     bubbleDown(idx){
-        const leftIdx = idx*2 + 1
-        const rightIdx = idx*2 + 2
+        const leftIdx=idx*2+1
+        const rightIdx=idx*2+2
+        let childIdx=idx
         
-        let childIdx = idx
-        if(leftIdx < this.heap.length && this.heap[leftIdx][0]<this.heap[childIdx][0])
-            childIdx = leftIdx
-        if(rightIdx < this.heap.length && this.heap[rightIdx][0]<this.heap[childIdx][0])
-            childIdx = rightIdx
+        if(leftIdx<this.getSize()&&this.heap[leftIdx][0]<this.heap[childIdx][0])
+            childIdx=leftIdx
+        if(rightIdx<this.getSize()&&this.heap[rightIdx][0]<this.heap[childIdx][0])
+            childIdx=rightIdx
         
-        if(childIdx == idx) return
-        
-        const tmp = this.heap[idx]
-        this.heap[idx] = this.heap[childIdx]
-        this.heap[childIdx] = tmp
-        
+        if(idx===childIdx) 
+            return  
+        [this.heap[childIdx],this.heap[idx]]=[this.heap[idx],this.heap[childIdx]]
         this.bubbleDown(childIdx)
-        
-        
-    }
-    heappush(pair){
-        this.heap.push(pair)
-        this.bubbleUp(this.heap.length-1)
-    }
-    heappop(){
-        if(this.heap.length==0) return null
-        if(this.heap.length==1) return this.heap.pop()
-        
-        const answer = this.heap[0]
-        this.heap[0] = this.heap.pop()
-        this.bubbleDown(0)
-        return answer
     }
     getSize(){
         return this.heap.length
     }
+    heappop() {
+        if (this.getSize() === 0) 
+            return null;
+        const answer = this.heap[0];
+        const lastOne = this.heap.pop();
+        if (this.getSize() > 0) {
+            this.heap[0] = lastOne;
+            this.bubbleDown(0);
+        }
+        return answer;
+    }
+    heappush(newElem){
+        this.heap.push(newElem)
+        const lastIdx = this.getSize()-1
+        this.bubbleUp(lastIdx)
+    }
 }
-
 function solution(N, road, K) {
-    const obj = {}
+    const heap=new Heap()
     
-    road.forEach(([a,b,c])=>{
-        if(!obj[a]) obj[a] = []
-        if(!obj[b]) obj[b] = []
-        
-        obj[a].push([c,b])
-        obj[b].push([c,a])
-    })
+    const obj={}
+    for(const [s,e,c] of road){
+        if(!obj[s])
+            obj[s]=[]
+        if(!obj[e])
+            obj[e]=[]
+        obj[s].push([c,e])
+        obj[e].push([c,s])
+    }
     
-    const heap = new MinHeap()
-    const distances = new Array(N+1).fill(10001*N)
-    distances[1] = 0
+    const distances=new Array(N+1).fill(COST_UPPER_LIMIT)
+    distances[1]=0
     heap.heappush([0,1])
     
-    while(heap.getSize()){
-        const [curCost, curNode] = heap.heappop()
-        
+    while(heap.getSize()!==0){
+        const [curCost,curNode]=heap.heappop()
         obj[curNode].forEach(([nxtCost, nxtNode])=>{
-            if(nxtCost+curCost < distances[nxtNode]){
-                distances[nxtNode] = nxtCost+curCost
+            if(nxtCost+curCost<distances[nxtNode]){
+                distances[nxtNode]=nxtCost+curCost
                 heap.heappush([nxtCost+curCost,nxtNode])
             }
         })
     }
-    
-    const answer = distances.filter((distance)=>distance<=K).length
-    return answer
+    return distances.filter((distance)=>distance<=K).length
 }
