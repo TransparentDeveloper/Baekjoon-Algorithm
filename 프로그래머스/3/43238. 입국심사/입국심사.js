9b2e2345bca71c47eee3a8699e5c6385f1c6bafb
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2024-10-18 14:37:27 +0900

    [level 3] Title: 입국심사, Time: 35.63 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,21 +1,20 @@
 function solution(n, times) {
-    let left = 0
-    let right = Math.max(...times) * n
+    let left=1;
+    let right=Math.max(...times) * n
     
-    
-    while(left <= right){
-        const mid = Math.floor((left+right)/2)
-        let people = 0
+    while(left<=right){
+        const mid=Math.floor((left+right)/2)
+        // 각 심사관이 mid 시간동안 처리할 수 있는 사람의 수
+        let completed=0
         
         for(const time of times)
-            people += Math.floor(mid / time)
+            completed+=Math.floor(mid/time)
         
-        if(people < n)
-            left = mid + 1
-        else 
-            right = mid -1    
+        if(completed<n)
+            left=mid+1
+        else
+            right=mid-1
     }
     
-    return left
-    
+    return left;
 }
