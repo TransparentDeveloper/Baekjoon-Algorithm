2626589893d83dc75e7342d974c943de005c86a1
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2024-10-24 21:38:45 +0900

    [level 3] Title: 합승 택시 요금, Time: 65.73 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,25 +1,30 @@
-const COST_UPPER_LIMIT=100000*200+1
+const MAX_COST = 200 * 100000 
 function solution(n, s, a, b, fares) {
-    const graph=Array.from({length:n+1},()=>new Array(n+1).fill(COST_UPPER_LIMIT))
-    for(let i=1;i<n+1;i++) graph[i][i]=0
-    fares.forEach(([start,end,cost])=>{
-        graph[start][end]=cost
-        graph[end][start]=cost
+    const graph = Array.from({length: n+1},()=>{
+        return new Array(n+1).fill(MAX_COST)
     })
-    for(let k=1;k<n+1;k++){
-        for(let i=1;i<n+1;i++){
+    for(let i =0; i<n+1; i++)
+        graph[i][i]=0
+    fares.forEach(([c,d,f])=>{
+        graph[c][d]=f
+        graph[d][c]=f
+    })
+    
+    
+    for(let k=1; k<n+1; k++){
+        for(let i=1; i<n+1; i++){
             for(let j=1;j<n+1;j++){
-                graph[i][j]=Math.min(graph[i][j],graph[i][k]+graph[k][j])
+                graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j])
             }
         }
     }
+    let answer = MAX_COST
+    for(let i=1;i<n+1;i++){
+        const commonCost = graph[s][i]
+        const aCost = graph[i][a]
+        const bCost = graph[i][b]
+        answer = Math.min(answer, aCost + bCost + commonCost)
+    }  
     
-    let answer=COST_UPPER_LIMIT
-    for(let point=1;point<n+1;point++){
-        const commonCost=graph[s][point]
-        const aCost=graph[point][a]
-        const bCost=graph[point][b]
-        answer=Math.min(answer,commonCost+aCost+bCost)
-    }
     return answer;
 }
