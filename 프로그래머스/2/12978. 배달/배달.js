e70227c6613d79ab88a03777d79d922183dd7f2e
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2024-10-24 22:11:59 +0900

    [level 2] Title: 배달, Time: 32.46 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,76 +1,73 @@
-const COST_UPPER_LIMIT=100000000000
-
-class Heap{
+class MinHeap{
     constructor(){
-        this.heap=[]
+        this.heap = []
     }
     bubbleUp(idx){
-        const parentIdx=Math.floor((idx-1)/2)
-        if(0<parentIdx&&this.heap[idx][0]<this.heap[parentIdx][0]){
-            [this.heap[idx],this.heap[parentIdx]]=[this.heap[parentIdx],this.heap[idx]]
+        const parentIdx = Math.floor((idx-1)/2)
+        if(parentIdx<0)return
+        if(this.heap[idx][0]<this.heap[parentIdx][0]){
+            this.swap(idx, parentIdx)
             this.bubbleUp(parentIdx)
         }
     }
     bubbleDown(idx){
-        const leftIdx=idx*2+1
-        const rightIdx=idx*2+2
-        let childIdx=idx
-        
-        if(leftIdx<this.getSize()&&this.heap[leftIdx][0]<this.heap[childIdx][0])
-            childIdx=leftIdx
-        if(rightIdx<this.getSize()&&this.heap[rightIdx][0]<this.heap[childIdx][0])
-            childIdx=rightIdx
-        
-        if(idx===childIdx) 
-            return  
-        [this.heap[childIdx],this.heap[idx]]=[this.heap[idx],this.heap[childIdx]]
-        this.bubbleDown(childIdx)
+        const leftIdx = idx*2 + 1
+        const rightIdx = idx*2 + 2
+        let targetIdx = idx
+        const size = this.getSize()
+        if(leftIdx<size&&this.heap[leftIdx][0]<this.heap[targetIdx][0])
+            targetIdx=leftIdx
+        if(rightIdx<size&&this.heap[rightIdx][0]<this.heap[targetIdx][0])
+            targetIdx=rightIdx
+        if(idx===targetIdx) return
+        this.swap(targetIdx,idx)
+        this.bubbleDown(targetIdx)
+    }
+    heapPush(newElem){
+        this.heap.push(newElem)
+        const size = this.getSize()
+        if(size!==1) {
+            this.bubbleUp(size-1)
+        }
+    }
+    heapPop(){
+        const size = this.getSize()
+        if(!size) return null
+        if(size==1) return this.heap.pop()
+        const answer = this.heap[0]
+        const lastElem = this.heap.pop()
+        this.heap[0]=lastElem
+        this.bubbleDown(0)
+        return answer
+    }
+    swap(idx1, idx2){
+        [this.heap[idx1],this.heap[idx2]] = [this.heap[idx2],this.heap[idx1]]
     }
     getSize(){
         return this.heap.length
     }
-    heappop() {
-        if (this.getSize() === 0) 
-            return null;
-        const answer = this.heap[0];
-        const lastOne = this.heap.pop();
-        if (this.getSize() > 0) {
-            this.heap[0] = lastOne;
-            this.bubbleDown(0);
-        }
-        return answer;
-    }
-    heappush(newElem){
-        this.heap.push(newElem)
-        const lastIdx = this.getSize()-1
-        this.bubbleUp(lastIdx)
-    }
 }
+const MAX_COST = 50*10000
 function solution(N, road, K) {
-    const heap=new Heap()
-    
-    const obj={}
-    for(const [s,e,c] of road){
-        if(!obj[s])
-            obj[s]=[]
-        if(!obj[e])
-            obj[e]=[]
-        obj[s].push([c,e])
-        obj[e].push([c,s])
-    }
-    
-    const distances=new Array(N+1).fill(COST_UPPER_LIMIT)
+    const obj = {}
+    road.forEach(([a,b,c])=> {
+        if(!obj[a]) obj[a]=[]
+        if(!obj[b]) obj[b]=[]
+        obj[a].push([c,b])
+        obj[b].push([c,a])
+    })
+    const minHeap = new MinHeap()
+    const distances=new Array(N+1).fill(MAX_COST)
     distances[1]=0
-    heap.heappush([0,1])
-    
-    while(heap.getSize()!==0){
-        const [curCost,curNode]=heap.heappop()
-        obj[curNode].forEach(([nxtCost, nxtNode])=>{
-            if(nxtCost+curCost<distances[nxtNode]){
-                distances[nxtNode]=nxtCost+curCost
-                heap.heappush([nxtCost+curCost,nxtNode])
+    minHeap.heapPush([0,1])
+    while(minHeap.getSize()){
+        const [curCost, curNode] = minHeap.heapPop()
+        for(const [nxtCost, nxtNode] of obj[curNode]){
+            if(nxtCost+curCost<distances[nxtNode]) {
+                distances[nxtNode] = nxtCost+curCost
+                minHeap.heapPush([nxtCost+curCost, nxtNode])
             }
-        })
+        }
     }
     return distances.filter((distance)=>distance<=K).length
 }
