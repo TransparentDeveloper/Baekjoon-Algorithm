3745fa2b04d6c601b1705ce50b23fa09d7aed496
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2024-10-27 15:51:29 +0900

    [Gold III] Title: 네 개의 소수, Time: 3680 ms, Memory: 20708 KB -BaekjoonHub

---
+++ 백준/Gold/1153. 네 개의 소수/네 개의 소수.js
@@ -0,0 +1,58 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+  input.push(line)
+}).on('close', function () {
+  const num = parseInt(input[0])
+  const answer = solution(num).join(' ')
+  console.log(answer)
+});
+
+function solution(num) {
+  const isPrime = aratos(num)
+  const primes = []
+  for (let elem = 2; elem <= num ; elem++)
+    if (isPrime[elem]) 
+      primes.push(elem)
+  
+  const primesSize = primes.length
+  for (let idx1 = 0; idx1 < primesSize; idx1++) {
+    const val1 = primes[idx1]
+    for (let idx2 = idx1; idx2 < primesSize; idx2++){
+      const val2 = primes[idx2]
+      if(val1 + val2 >= num) continue
+      for (let idx3 = idx2; idx3 < primesSize; idx3++){
+        const val3 = primes[idx3]
+        if(val1 + val2+ val3 >= num) continue
+        for (let idx4 = idx3; idx4 < primesSize; idx4++){
+          const val4 = primes[idx4]
+          if (val1 + val2 + val3+ val4 === num) 
+            return [val1, val2, val3, val4]
+        }
+      }
+    }
+  }
+  
+  return [-1]
+}
+
+function aratos(upperLimit) {
+  const isPrime = new Array(upperLimit + 1).fill(true) 
+  isPrime[0] = false
+  isPrime[1] = false
+
+  const sqrt = Math.sqrt(upperLimit)
+
+  for (let i = 2; i < sqrt; i++){
+    if (!isPrime[i]) continue
+    for (let j = i * i; j <= upperLimit; j += i) 
+      isPrime[j] = false
+  }
+
+  return isPrime
+}
