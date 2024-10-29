287b137f240c32940dd2d655a9ea61384d4278c6
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2024-10-30 07:57:17 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.13 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,13 +1,15 @@
 function solution(arr) {
-    return arr.reduce(getLCM)
+    const answer = arr.reduce(getLCM)
+    return answer;
 }
+
 function getLCM(a,b){
-    let max=Math.max(a,b)
-    let min=Math.min(a,b)
-    while (min!==0){
-        const mod=max%min
-        max=min
-        min=mod
+    let max = Math.max(a,b)
+    let min = Math.min(a,b)
+    while(min!==0){
+        const mod = max%min
+        max = min
+        min = mod
     }
     return (a*b)/max
 }
