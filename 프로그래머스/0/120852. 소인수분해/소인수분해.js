bc74942a0b8b85bc0a5bb73a02c8d37053ec5429
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2024-11-09 12:15:04 +0900

    [level 0] Title: 소인수분해, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -1,22 +1,19 @@
 function solution(n) {
-    const set = new Set()
-    const primeFactors = primeFactorization(n)
-    primeFactors.forEach((factor)=>{
-        set.add(factor)
-    })
+    const set = new Set(getPrimeDivisors(n))
     return Array.from(set)
 }
 
-function primeFactorization(n){
-    const factors = []
+function getPrimeDivisors(n){
+    const answers = []
     let divisor = 2
     
-    while(n>1){
-        if(n % divisor === 0){
-            factors.push(divisor)
+    while (n>1){
+        if(n % divisor == 0){
+            answers.push(divisor)
             n /= divisor
-        } else divisor++
+        }
+        else
+            divisor ++
     }
-    
-    return factors
+    return answers
 }
