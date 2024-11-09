422c76827b02064ca72731a85ecb530286717326
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2024-11-09 11:44:43 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,11 +1,11 @@
 function solution(n, m) {
     const gcd = getGCD(n,m)
-    const lcm = (n*m)/gcd
-    return [gcd, lcm]
+    const lcm = (n*m) / getGCD(n,m)
+    return [gcd, lcm];
 }
 
-function getGCD(a, b){
-    const max = Math.max(a,b)
+function getGCD(a,b){
     const min = Math.min(a,b)
-    return min==0? max:getGCD(min,max%min)
+    const max = Math.max(a,b)
+    return min == 0 ? max : getGCD(min, max%min)
 }
