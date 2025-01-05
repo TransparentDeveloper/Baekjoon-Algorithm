483fa6cc0fc4a83fc1b180992fc49a2b8739a8f9
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2025-01-05 19:35:45 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -2,15 +2,13 @@ function solution(arr) {
     return arr.reduce(getLCM)
 }
 
-function getLCM (a,b) {
-    let max = Math.max(a,b)
-    let min = Math.min(a,b)
-    
-    while (min !== 0) {
-        const mod = max % min
-        max = min
-        min = mod
-    }
+function getLCM(a,b) {
+    const gcd = getGCD(a,b)
+    return (a*b) / gcd
+}
 
-    return (a*b) / max
+function getGCD(a,b) {
+    const max = Math.max(a,b)
+    const min = Math.min(a,b)
+    return min === 0 ? max : getGCD(min, max%min)
 }
--- 프로그래머스/2/12953. N개의 최소공배수/README.md
+++ 프로그래머스/2/12953. N개의 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 2] N개의 최소공배수 - 12953 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.18 ms
+메모리: 33.4 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 03일 22:36:45
+2025년 01월 05일 19:35:42
 
 ### 문제 설명
 
